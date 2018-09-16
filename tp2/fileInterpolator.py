10f237c4b4eae77efa6fd646951ade0da8e99d9c
florvimberg <florencia.vimberg@ing.austral.edu.ar>
2018-09-15 21:37:09 -0300

    Algorithm added, desnt well correctly

--- tp2/fileInterpolator.py
+++ tp2/fileInterpolator.py
@@ -9,34 +9,40 @@ def csvCreator():
 
     reader = csv.reader(file)
 
-    w, h = 10, 5000
-    # Matrix = [[0 for x in range(w)] for y in range(h)]
-
-    heights = []
-    latitudes = []
-    longitudes = []
+    heights = [[0] * 50 for i in range(10)]
+    latitudes = [[0] * 50 for i in range(10)]
+    longitudes = [[0] * 50 for i in range(10)]
 
     count = 0
     # count va a ir de 0 a 500, por lo cual vamos a tener 10 sub arreglos
     for row in reader:
-        if count <= 50:
-            heights.append(float(row[0]))
-            latitudes.append(float(row[1]))
-            longitudes.append(float(row[2]))
+        position = int(count / 50)
+        innerPosition = count % 50
+        heights[position][innerPosition] = float(row[0])
+        latitudes[position][innerPosition] = float(row[1])
+        longitudes[position][innerPosition] = float(row[2])
+        count += 1
 
-            count += 1
-
-    coefficientsLat = getCoefficients(np.array(heights), np.array(latitudes))
-    coefficientsLong = getCoefficients(np.array(heights), np.array(longitudes))
+    coefficientsLat = []
+    coefficientsLong = []
+    for i in range(0, 10, 1):
+        if i == 9:
+            coefficientsLat.append(getCoefficients(np.array(heights[i][:-9 or None]), np.array(latitudes[i][:-9 or None])))
+            coefficientsLong.append(getCoefficients(np.array(heights[i][:-9 or None]), np.array(longitudes[i][:-9 or None])))
+        else:
+            coefficientsLat.append(getCoefficients(np.array(heights[i]), np.array(latitudes[i])))
+            coefficientsLong.append(getCoefficients(np.array(heights[i]), np.array(longitudes[i])))
 
     file = open("files/interpolation.csv", 'w')
 
     file.write("Altura, Latitud, Longitud \n")
 
-    for i in range(50000, 45000, -1):
+    for i in range(50000, -1, -1):
+        index = 9 - int(i/5000)
+
         file.write(str(i) + ",")
-        file.write(str(evaluate(np.array(coefficientsLat), np.array(heights), i)) + ",")
-        file.write(str(evaluate(np.array(coefficientsLong), np.array(heights), i)) + "\n")
+        file.write(str(evaluate(np.array(coefficientsLat[index]), np.array(heights[index]), i)) + ",")
+        file.write(str(evaluate(np.array(coefficientsLong[index]), np.array(heights[index]), i)) + "\n")
 
 if __name__ == '__main__':
     csvCreator()
