e7ed00b7d88dafa695d9ac68bda7df467081c35e
tomas.iturralde <tomas.iturralde@ing.austral.edu.ar>
2018-09-16 20:17:09 -0300

    tp2 advanced

--- tp2/grapher.py
+++ tp2/grapher.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import csv
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def printGraphALAT():
@@ -52,6 +53,36 @@ def printGraphALON():
     print("finished")
 
 
+def printGraph():
+    file = open("files/positions.csv", 'r')
+
+    reader = csv.reader(file)
+    fig = plt.figure()
+    ax = Axes3D(fig)
+
+    xValues = []
+    yValues = []
+    zValues = []
+
+    next(reader)
+
+    for row in reader:
+        xValues.append(row[2])  # longitud
+        yValues.append(row[1])  # latitud
+        zValues.append(row[0])  # altura
+
+    x = list(map(float, xValues))
+    y = list(map(float, yValues))
+    z = list(map(float, zValues))
+    ax.scatter3D(x, y, z, cmap='Greens')
+    ax.set_title('Altura, Latitud, Longitud')
+    ax.set_xlabel('longitud')
+    ax.set_ylabel('latitud')
+    ax.set_zlabel('altura')
+
+    plt.show()
+    print("finished")
+
+
 if __name__ == '__main__':
-    printGraphALAT()
-    printGraphALON()
+    printGraph()
